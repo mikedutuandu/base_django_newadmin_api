3b86890a3e6c58be7ea5a6cec5a359a04c957b77
Binh Le <mikedutuandu@gmail.com>
2018-12-17 22:14:59 +0700

    -add check permission for each view

--- apps/admins/views/users.py
+++ apps/admins/views/users.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import DetailView,ListView
 from apps.fronts.models import User
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required,permission_required
 from django.utils.decorators import method_decorator
 
 
-@method_decorator([login_required(login_url="/admin/login")], name='dispatch')
+@method_decorator([login_required(login_url="/admin/login"),permission_required('fronts.view_user', raise_exception=True)], name='dispatch')
 class UserListView(ListView):
     queryset = User.objects.all()
     template_name = 'users/list.html'
